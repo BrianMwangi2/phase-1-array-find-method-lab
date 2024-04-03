ede19fb4aa7e26a2ed50d2539c4cc9adb15f90cc
Brian <brian.mwangi2@student.moringaschool.com>
2024-04-03 13:42:35 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+    return winRecord ? winRecord.year : undefined;
+}
+// 1. made function  called superbowlWin that takes in an array of objects (called records). Each object represents a
+
